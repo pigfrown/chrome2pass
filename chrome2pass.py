4218a35a28c0aee26210bcec5b646ac774d7967f
Pig Frown <pigfrown@protonmail.com>
2022-11-25 16:24:22 +0000

    chore: fixes formatting, adds header/shebang

--- chrome2pass.py
+++ chrome2pass.py
@@ -1,8 +1,13 @@
+#!/usr/bin/env python3
+
+# Author: Pig Frown <pigfrown@protonmail.com>
+# Copyright 2022 Pig Frown
+# This file is licensed under the MIT License. See LICENSE in repository
+
 import subprocess
 import argparse
 import sys
 import csv
-
 
 
 class ArgParser(argparse.ArgumentParser):
@@ -10,6 +15,7 @@ class ArgParser(argparse.ArgumentParser):
         print(msg, file=sys.stderr)
         self.print_help()
         sys.exit(1)
+
 
 def confirmed(site, username=None):
     if username is None:
@@ -33,6 +39,7 @@ def confirmed(site, username=None):
 
         print("Please enter y or n")
 
+
 def import_password(site, user, password):
     path = f"{site}/{user}"
     pass_input = f"{password}\n".encode()
@@ -50,12 +57,15 @@ def import_password(site, user, password):
     if returned != 0:
         print(f"ERROR: could not import {site}.. pass returned {returned}")
 
+
 def main():
     description = "Import pass entries from Chrome based browser export CSV"
     parser = ArgParser(description=description)
 
     parser.add_argument("csv_file", help="The exported Chrome CSV file")
-    parser.add_argument("--confirm", action="store_true", help="Ask for confirmation before importing each password")
+    parser.add_argument("--confirm",
+                        action="store_true",
+                        help="Ask for confirmation before importing each password")
 
     args = parser.parse_args()
 
@@ -65,7 +75,6 @@ def main():
         if args.confirm:
             if not confirmed(row["name"], row["username"]):
                 continue
-
 
         import_password(row["name"], row["username"], row["password"])
 
